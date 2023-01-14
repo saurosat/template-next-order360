ebb845b09c92fab2ebf377ec695655b4011f331f
saurosat <tuan.nguyen.1110@gmail.com>
2023-01-14 10:39:26 +0700

    enable nftTracing to bypass ENOENT error on Vercel

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   images: {
     domains: ['i.pravatar.cc', 'picsum.photos'],
   },
+
   //Apply fix patch for process.cwd() issue as per https://github.com/vercel/next.js/issues/8251#issuecomment-915287535
-  /**
-  experimental: { 
-    nftTracing: true 
-  } */
+  experimental: {
+    nftTracing: true
+  }
 }
 
 module.exports = nextConfig
